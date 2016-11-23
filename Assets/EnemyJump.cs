025947ad9ad5b87cb07d962414eebf546659d5b9
lucasmuileboom <muileboomlucas@gmail.com>
2016-11-23 16:26:22 +0100

    enemy

---
+++ Assets/EnemyJump.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyJump : MonoBehaviour 
+{
+	public int jumpPower;
+	float timeLeft = 3;
+
+	void Update () 
+	{
+		timeLeft -= Time.deltaTime;
+		if(timeLeft < 0)
+		{
+			jump();
+		}
+	}
+	void jump ()
+	{
+		GetComponent<Rigidbody2D>().AddForce(transform.up * jumpPower * Time.deltaTime);
+		timeLeft = 10;
+	}
+}
---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour 
+{
+	public float Speed;
+	public int MinX;
+	public int MaxX;
+
+	bool Move = true;
+
+	void Update () 
+	{
+		if (MaxX < this.transform.position.x)
+		{
+			Move = true;
+		}
+		if (MinX > this.transform.position.x)
+		{
+			Move = false;
+		}
+
+		if (Move == true) 
+		{
+			transform.Translate (Vector3.left * Speed * Time.deltaTime);
+		}
+		if (Move == false) 
+		{
+			transform.Translate (Vector3.right * Speed * Time.deltaTime);
+		}
+	}
+}
